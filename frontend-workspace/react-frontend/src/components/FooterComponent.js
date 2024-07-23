948350684c718401ba2c21bd05ac0a841cf0e087
yangalareddyrajashekarreddy <145536269+yangalareddyrajashekarreddy@users.noreply.github.com>
2024-07-23 14:01:19 +0530

    Update FooterComponent.js

--- frontend-workspace/react-frontend/src/components/FooterComponent.js
+++ frontend-workspace/react-frontend/src/components/FooterComponent.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
     	return (
       	<div>
         	<footer className='footer'>
-       <span className='text-muted'>All Right Reserved 2024 &copy;NIYAZ_SIR </span>
+       <span className='text-muted'>All Right Reserved 2024 &copy;Yangalareddy Rajashekar Reddy </span>
         </footer>
      	 </div>
     	)
