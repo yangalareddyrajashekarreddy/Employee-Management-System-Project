fa1e36456348a7e8e2d8129cc3296ef6ab2f51c1
yangalareddyrajashekarreddy <yangalareddyrajashekarreddy@gmail.com>
2024-07-23 13:47:46 +0530

    my project

---
+++ backend-workspace/backend-springboot/src/main/java/com/ihub/www/model/Employee.java
@@ -0,0 +1,63 @@
+package com.ihub.www.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee 
+{
+	
+		@Id 
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private long id;
+		
+		@Column
+		private String firstName;  
+		
+		@Column
+		private String lastName;  
+		
+		@Column
+		private String email;
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		
+	
+
+}
---
+++ frontend-workspace/react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+	export default class FooterComponent extends Component {
+ 	 render() {
+    	return (
+      	<div>
+        	<footer className='footer'>
+       <span className='text-muted'>All Right Reserved 2024 &copy;NIYAZ_SIR </span>
+        </footer>
+     	 </div>
+    	)
+  	}
+	}
